7c1307cbf501d1ae50d76385160a490e534934dc
yusato1022 <y10clk@gmail.com>
2026-02-01 00:54:00 +0900

    change api

--- api/chat.js
+++ api/chat.js
@@ -1,8 +1,8 @@
 export default function handler(req, res) {
   res.status(200).json({
-    keyExists: !!process.env.OPENAI_API_KEY,
-    keyLength: process.env.OPENAI_API_KEY
-      ? process.env.OPENAI_API_KEY.length
+    keyExists: !!process.env.OPENAI_API_KEY_M5,
+    keyLength: process.env.OPENAI_API_KEY_M5
+      ? process.env.OPENAI_API_KEY_M5.length
       : 0
   });
 }
